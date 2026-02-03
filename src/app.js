d9b6a75428bf2584d674ce3329574d0aca5e7dbe
Kanak Rawat <kanakrawat1910@gmail.com>
2026-02-03 17:06:48 +0530

    get ,post , delete apis

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
-
 const express = require("express");
 
 const app = express();
+app.use("/user", (req, res) => {
+  res.send(" This is your first server...");
+});
+// get api
+app.get("/user", (req, res) => {
+  res.send({ firstName: "kanak", lastName: "rawat" });
+});
+
+//  post api
+app.post("/user", (req, res) => {
+  console.log(" save data to the DB");
+  res.send(" data successfully saved to the database");
+});
+// delete api
+app.delete("/user", (req, res) => {
+  res.send(" data successfully delete");
+});
+
 app.use("server", (req, res) => {
   res.send(" This is your first server...");
 });
@@ -9,10 +26,9 @@ app.use("server", (req, res) => {
 app.use("/hello", (req, res) => {
   res.send(" Hello from the request hello with NODEMON..");
 });
-app.use("/test",(req,res )=>{
-    res.send ( " Hello from the server with NODEMON..");
+app.use("/test", (req, res) => {
+  res.send(" Hello from the server with NODEMON..");
 });
-app.listen(3000, ()=>{
-    console.log(" server is successfully listening on port 3000.....");
-
+app.listen(3000, () => {
+  console.log(" server is successfully listening on port 3000.....");
 });
