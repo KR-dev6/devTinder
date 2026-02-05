6ae696dafa3dfd935f024c510c42a3650d160681
Kanak Rawat <kanakrawat1910@gmail.com>
2026-02-05 19:59:14 +0530

    multiple route-handlers

--- src/app.js
+++ src/app.js
@@ -1,33 +1,29 @@
 const express = require("express");
 
 const app = express();
-app.use("/user", (req, res) => {
-  res.send(" This is your first server...");
-});
-// get api
-app.get("/user", (req, res) => {
-  res.send({ firstName: "kanak", lastName: "rawat" });
-});
 
-//  post api
-app.post("/user", (req, res) => {
-  console.log(" save data to the DB");
-  res.send(" data successfully saved to the database");
-});
-// delete api
-app.delete("/user", (req, res) => {
-  res.send(" data successfully delete");
-});
 
-app.use("server", (req, res) => {
-  res.send(" This is your first server...");
-});
 
-app.use("/hello", (req, res) => {
-  res.send(" Hello from the request hello with NODEMON..");
+// Multiple route handler using next() fn
+
+app.use("/user", (req, res,next) => {
+  console.log(" 1st route  running properly");
+  next();
+  // res.send("1st response");
 });
-app.use("/test", (req, res) => {
-  res.send(" Hello from the server with NODEMON..");
+app.use("/user", (req, res,next) => {
+  console.log(" 2nd route  running properly");
+  next();
+  // res.send("2nd response");
+});
+app.use("/user", (req, res,next) => {
+  console.log(" 3rd route  running properly");
+  next();
+  // res.send("3rd response");
+});
+app.use("/user", (req, res, ) => {
+  console.log(" 4th route  running properly");
+  res.send("4th response");
 });
 app.listen(3000, () => {
   console.log(" server is successfully listening on port 3000.....");
